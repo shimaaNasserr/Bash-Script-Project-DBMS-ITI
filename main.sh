29afe270bc3f14959ed2252bfc3a9291cc9596c8
shimaaNasserr <shimaanasser304@gmail.com>
2025-05-30 15:56:21 +0300

    create table files for meta and data

--- main.sh
+++ main.sh
@@ -35,13 +35,60 @@ while true; do
                     select table_action in "Create Table" "List Tables" "Drop Table" "Back to Main Menu"
                     do
                     case $table_action in
-                        "Create Table")
-                        echo "Enter table name:"
-                        read tablename
-                        touch "$DB_DIR/$dbname/$tablename"
-                        echo "Table '$tablename' created."
-                        break
-                        ;;
+                    #----------
+                    "Create Table")
+                     echo "Enter Table Name:"
+                     read tablename
+                    table_file="$DB_DIR/$dbname/$tablename"
+                    meta_file="$table_file.meta"
+
+                    if [ -f "$table_file" ]; then
+                      echo "Table already exists."
+                    else
+                     echo "How many columns?"
+                      read col_count
+
+                      columns=() # array store column names
+                      datatypes=() # array store column datatypes
+                      pk_set=false 
+
+                    for (( i=1; i<=col_count; i++ ))
+                    do
+                    echo "Enter name of column $i:"
+                    read col_name
+
+                    echo "Enter datatype of $col_name (string/number):"
+                    read col_type
+
+                    # check if datatype is valid or not
+                    while [[ "$col_type" != "string" && "$col_type" != "number" ]]; do
+                     echo "Invalid datatype. Enter string or number:"
+                     read col_type
+                    done
+
+                    echo "Is this column the Primary Key? (yes/no):"
+                    read is_pk
+
+                   if [[ "$is_pk" == "yes" && "$pk_set" == false ]]; then
+                      columns+=("$col_name:$col_type:pk")
+                      pk_set=true
+                   else
+                   columns+=("$col_name:$col_type")
+                   fi
+                   done
+
+                    # Create file for data to insert into it 
+                    touch "$table_file"
+
+                    # Write meta file 
+                    for col in "${columns[@]}"; do
+                        echo "$col" >> "$meta_file"
+                    done
+
+                        echo "Table '$tablename' created with metadata."
+                    fi
+                    break
+                    ;;
                         "List Tables")
                         echo "Tables in '$dbname':"
                         ls "$DB_DIR/$dbname"
